bf3a1ead69f8830e60adc2301b1f7b6afd97f8d2
Christiano0407 <placebo0704@gmail.com>
2022-06-07 10:06:13 -0500

    project API 02

--- src/js/index.js
+++ src/js/index.js
@@ -5,12 +5,13 @@ const overlay = document.getElementById("[data-overlay]");
 const navBar = document.getElementById("navId");
 
 //*? == ADD Function active Event ==  */
-const addElements = [btnMenuOpen, menuClose, overlay];
+const addOpen = [btnMenuOpen, menuClose, overlay];
 
-for (let i = 0; i < addElements.length; i++) {
-  addElements[i].addEventListener("click", () => {
+/* for (let i = 0; i < addElements.length; i++) {
+  addOpen[i].addEventListener("click", function () {
     navBar.classList.toggle("active");
     overlay.classList.toggle("active");
     document.body.classList.toggle("active");
   });
 }
+ */
