4fc875b27597a793a4137e900d6d0e0e02c4f139
Christiano0407 <placebo0704@gmail.com>
2022-06-07 12:34:06 -0500

    API Project

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,8 @@
-//** == All Class ==  */
-
+//** == All Class / Import  ==  */
 import { btnMenuOpen } from "./node.js";
 import { navBar } from "./node.js";
 
+//*TODO:  ==== All Events Movie App ====  */
 btnMenuOpen.addEventListener("click", () => {
   console.log("Btn");
 
